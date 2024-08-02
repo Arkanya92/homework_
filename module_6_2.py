6ff08df3e9b9aad124c90b75e999e63c5898effc
Arkanya92 <studentgang@gmail.com>
2024-08-02 23:26:39 +0300

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -7,29 +7,30 @@ class Vehicle:
         self.__color = __color
 
     def get_model(self):
-        print(f'Модель: {self.__model}')
+        return f'Модель: {self.__model}'
 
     def get_horsepower(self):
-        print(f'Мощность двигателя: {self.__engine_power}')
+        return f'Мощность двигателя: {self.__engine_power}'
 
     def get_color(self):
-        print(f'Цвет: {self.__color}')
+        return f'Цвет: {self.__color}'
 
     def print_info(self):
-        self.get_model()
-        self.get_horsepower()
-        self.get_color()
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
         print(f'Владелец: {self.owner}')
 
     def set_color(self, new_color):
-        self.new_color = new_color
         if new_color.lower() in self.__COLOR_VARIANTS:
             self.__color = new_color
         else:
-            (f'Нельзя сменить цвет на {new_color}')
+            print(f'Нельзя сменить цвет на {new_color}')
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
+    def __init__(self, owner, __model, __color, __engine_power):
+        super().__init__(owner, __model, __color, __engine_power)
 
 
 # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
